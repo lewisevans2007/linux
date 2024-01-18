9638450b13bb8c935df06b0d4a18cf592472b2fa
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-01-18 13:25:34 +0000

    arch: add assembly files
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

---
+++ kernel/bounds.s
@@ -0,0 +1,52 @@
+	.arch armv8-a
+	.file	"bounds.c"
+// GNU C11 (Debian 12.2.0-14) version 12.2.0 (aarch64-linux-gnu)
+//	compiled by GNU C version 12.2.0, GMP version 6.2.1, MPFR version 4.1.1-p1, MPC version 1.3.1, isl version isl-0.25-GMP
+
+// warning: MPFR header version 4.1.1-p1 differs from library version 4.2.0.
+// GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+// options passed: -mlittle-endian -mgeneral-regs-only -mabi=lp64 -mbranch-protection=none -Os -std=gnu11 -fshort-wchar -funsigned-char -fno-common -fno-PIE -fno-strict-aliasing -fno-asynchronous-unwind-tables -fno-unwind-tables -fno-delete-null-pointer-checks -fno-allow-store-data-races -fno-stack-protector -fno-omit-frame-pointer -fno-optimize-sibling-calls -ftrivial-auto-var-init=zero -fno-stack-clash-protection -falign-functions=4 -fno-strict-overflow -fstack-check=no -fconserve-stack
+	.text
+	.section	.text.startup,"ax",@progbits
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+// kernel/bounds.c:19: 	DEFINE(NR_PAGEFLAGS, __NR_PAGEFLAGS);
+#APP
+// 19 "kernel/bounds.c" 1
+	
+.ascii "->NR_PAGEFLAGS 22 __NR_PAGEFLAGS"	//
+// 0 "" 2
+// kernel/bounds.c:20: 	DEFINE(MAX_NR_ZONES, __MAX_NR_ZONES);
+// 20 "kernel/bounds.c" 1
+	
+.ascii "->MAX_NR_ZONES 4 __MAX_NR_ZONES"	//
+// 0 "" 2
+// kernel/bounds.c:22: 	DEFINE(NR_CPUS_BITS, ilog2(CONFIG_NR_CPUS));
+// 22 "kernel/bounds.c" 1
+	
+.ascii "->NR_CPUS_BITS 8 ilog2(CONFIG_NR_CPUS)"	//
+// 0 "" 2
+// kernel/bounds.c:24: 	DEFINE(SPINLOCK_SIZE, sizeof(spinlock_t));
+// 24 "kernel/bounds.c" 1
+	
+.ascii "->SPINLOCK_SIZE 4 sizeof(spinlock_t)"	//
+// 0 "" 2
+// kernel/bounds.c:29: 	DEFINE(LRU_GEN_WIDTH, 0);
+// 29 "kernel/bounds.c" 1
+	
+.ascii "->LRU_GEN_WIDTH 0 0"	//
+// 0 "" 2
+// kernel/bounds.c:30: 	DEFINE(__LRU_REFS_WIDTH, 0);
+// 30 "kernel/bounds.c" 1
+	
+.ascii "->__LRU_REFS_WIDTH 0 0"	//
+// 0 "" 2
+// kernel/bounds.c:35: }
+#NO_APP
+	mov	w0, 0	//,
+	ret	
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
